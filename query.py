22a4051999622c9cfaf4ac443e2c7297b9aec87f
Eric Berquist <eric.berquist@gmail.com>
2020-01-14 21:00:41 -0500

    Initial test query

---
+++ query.py
@@ -0,0 +1,23 @@
+import json
+import os
+from pathlib import Path
+
+import requests
+
+# adapted from https://stackoverflow.com/a/46271487
+api_token_envvar = "GITHUB_API_TOKEN"
+api_token = os.getenv(api_token_envvar, None)
+if not api_token:
+    raise RuntimeError(
+        f"GitHub API token not stored in env variable ${api_token_envvar}"
+    )
+url = 'https://api.github.com/graphql'
+# query = { 'query' : '{ viewer { repositories(first: 30) { totalCount pageInfo { hasNextPage endCursor } edges { node { name } } } } }' }
+query = { 'query': Path("./query.graphql").read_text() }
+headers = {'Authorization': f'token {api_token}'}
+
+r = requests.post(url=url, json=query, headers=headers)
+result = json.loads(r.text)
+with open("result.json", "w") as handle:
+    json.dump(result, handle)
+print(result)
